b6b50cdf1c73de3a98ac2442e1339725bd249c6d
thanh <hoainhien0908@gmail.com>
2020-02-22 15:16:43 +0700

    Show real shops and owners counts in homepage

--- src/main/java/com/crm/cico/controllers/IndexController.java
+++ src/main/java/com/crm/cico/controllers/IndexController.java
@@ -1,16 +1,23 @@
 package com.crm.cico.controllers;
 
+import com.crm.cico.services.OwnerService;
+import com.crm.cico.services.ShopService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@AllArgsConstructor
 @Controller
 public class IndexController {
 
+    private ShopService shopService;
+    private OwnerService ownerService;
+
     @RequestMapping({"", "/", "index"})
     public String getIndexPage(Model model) {
-        model.addAttribute("totalShopOwners", 1000);
-        model.addAttribute("totalShops", 2000);
+        model.addAttribute("totalShopOwners", shopService.count());
+        model.addAttribute("totalShops", ownerService.count());
         return "index";
     }
 }
