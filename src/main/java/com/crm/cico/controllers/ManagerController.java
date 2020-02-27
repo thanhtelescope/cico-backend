0fcb0a445fd6d29f645ac63d9562e36c05853f32
thanh <hoainhien0908@gmail.com>
2020-02-27 09:01:36 +0700

    Show manager profile #26

--- src/main/java/com/crm/cico/controllers/ManagerController.java
+++ src/main/java/com/crm/cico/controllers/ManagerController.java
@@ -1,10 +1,15 @@
 package com.crm.cico.controllers;
 
+import com.crm.cico.model.Manager;
 import com.crm.cico.services.ManagerService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @AllArgsConstructor
 @Controller
@@ -17,5 +22,15 @@ public class ManagerController {
     public String getIndexPage(Model model) {
         model.addAttribute("managers", managerService.findAll());
         return "managers/index";
+    }
+
+    @GetMapping("/{managerId}")
+    public String showOwner(@PathVariable Long managerId, Model model) {
+        Optional<Manager> manager = managerService.findById(managerId);
+        if (!manager.isPresent()) {
+            throw new RuntimeException("Manager not exists");
+        }
+        model.addAttribute("manager", manager.get());
+        return "managers/managerDetails";
     }
 }
